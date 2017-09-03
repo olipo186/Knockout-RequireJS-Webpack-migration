4755391101a6d6589cc4ba93f239b326e336f52a
Oliver Poignant <oliver@poignant.se>
2017-09-03 21:30:13 +0200

    Lazy load of components

--- knockout-requirejs-example/app/components.js
+++ knockout-requirejs-example/app/components.js
@@ -2,9 +2,9 @@ define(function (require) {
 
     var ko = require("knockout");
 
-    ko.components.register("index-page", require('app/components/index-page'));
-    ko.components.register("main-page", require('app/components/main-page'));
-    ko.components.register("contact-page", require('app/components/contact-page'));
-    ko.components.register("message-widget", require('app/components/message-widget'));
+    ko.components.register("index-page", { require: 'app/components/index-page' });
+    ko.components.register("main-page", { require: 'app/components/main-page' });
+    ko.components.register("contact-page", { require: 'app/components/contact-page' });
+    ko.components.register("message-widget", { require: 'app/components/message-widget' });
     
 });
